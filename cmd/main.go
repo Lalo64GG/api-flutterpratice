73ab6f433e2037c3d7bf303a4c235294d565b2a9
Dariohg <rubendarioh99@gmail.com>
2025-06-04 01:29:02 -0600

    feat: Refactor server initialization and environment configuration

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"api/cmd/server"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
-var (
-	HOST = os.Getenv("HOST_SERVER")
-	PORT = os.Getenv("PORT_SERVER")
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 
-)
+	host := os.Getenv("HOST_SERVER")
+	port := os.Getenv("PORT_SERVER")
+	useHTTPS := os.Getenv("USE_HTTPS") == "true"
+	certFile := os.Getenv("SSL_CERT_FILE")
+	keyFile := os.Getenv("SSL_KEY_FILE")
 
-func main(){
-	srv := server.NewServer(HOST, PORT)
+	srv := server.NewServer(host, port, useHTTPS, certFile, keyFile)
 	srv.Run()
 }
